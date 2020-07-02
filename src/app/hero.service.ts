15d247b14539879188b2369b2e82bc1dac8d0893
Romain Dautel <romaindaut@freegypsies.com>
2020-07-02 17:18:19 +0100

    Added Messages on action called

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@angular/core';
 import {HEROES} from "./mock-heroes";
 import {Hero} from "./hero";
 import {Observable, of} from "rxjs";
+import {MessageService} from "./messages.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   getHeroes(): Observable <Hero[]> {
+    //TODO send message after fetching heroes
+    this.messageService.add('HeroServices: fetch heroes')
     return of (HEROES)
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
